5a2ef765819764c533a51247b8cd0f3bb2402c5f
Juraj Nyiri <jnyiri@sk.ibm.com>
2025-06-03 01:22:38 +0200

    Fix: AlexxIT/WebRTC:851

--- custom_components/webrtc/www/webrtc-camera.js
+++ custom_components/webrtc/www/webrtc-camera.js
@@ -76,9 +76,9 @@ class WebRTCCamera extends VideoRTC {
 
     set hass(hass) {
         this._hass = hass;
-        this.onhass.forEach(fn => fn());
+        //this.onhass.forEach(fn => fn());
         // if card in vertical stack - `hass` property assign after `onconnect`
-        // this.onconnect();
+        this.onconnect();
     }
 
     get hass() {
